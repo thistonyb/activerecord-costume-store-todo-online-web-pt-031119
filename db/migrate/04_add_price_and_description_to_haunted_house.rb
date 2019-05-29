2195ba5f122163719a8e8d03b23c269a53451b70
Tony Baker <this.tonybaker@gmail.com>
2019-05-29 21:53:39 +0000

    Automatically backed up by Learn

--- db/migrate/04_add_price_and_description_to_haunted_house.rb
+++ db/migrate/04_add_price_and_description_to_haunted_house.rb
@@ -1,6 +1,7 @@
 class AddPriceAndDescriptionToHauntedHouse < ActiveRecord::Migration[5.2]
   def change
     add_column(:haunted_houses, :price, :float)
+    add_column(:haunted_houses, :description, :text)
   end
 
 end
