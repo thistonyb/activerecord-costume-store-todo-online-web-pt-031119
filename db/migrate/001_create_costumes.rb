cab8950de82767e0d54867da8af236a8683fcdee
Tony Baker <this.tonybaker@gmail.com>
2019-05-29 21:24:32 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -10,6 +10,11 @@
 class CreateCostumes < ActiveRecord::Migration
   def change
     create_table :costumes do |t|
-      t.string :name
+      t.text :name
+      t.float :price
+      t.integer :size
+      t.text :image_url
+      t.timestamps
+    end
   end
 end
